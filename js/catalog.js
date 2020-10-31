ca622f0f036d02c5d2829c841be974816f80cbb5
Ammen7 <68874306+Ammen7@users.noreply.github.com>
2020-10-31 22:19:54 +1000

    Оживление формы

--- js/catalog.js
+++ js/catalog.js
@@ -1,18 +1,19 @@
-let cartPopup = document.querySelector('.cart-popup');
-let cartButtonClose = document.querySelector('.cart-popup-button-close');
-let cartButtonContinue = document.querySelector('.cart-popup-continue');
-let cartButtonOpen = document.querySelector('.popup-button-buy');
+const cartPopup = document.querySelector('.cart-popup');
+const cartButtonClose = document.querySelector('.cart-popup-button-close');
+const cartButtonContinue = document.querySelector('.cart-popup-continue');
+const cartButtonOpenList = document.querySelector('.popup-button-buy');
 
-cartButtonOpen.onclick = function (evt) {
+cartButtonOpenList.addEventListener("click", function (evt) {
   evt.preventDefault();
   cartPopup.classList.add('show');
-};
+});
 
-cartButtonClose.onclick = function () {
-  cartPopup.classList.remove('show');
-};
-
-cartButtonContinue.onclick = function (evt) {
+cartButtonClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   cartPopup.classList.remove('show');
-};
+});
+
+cartButtonContinue.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cartPopup.classList.remove('show');
+});
--- js/index.js
+++ js/index.js
@@ -1,27 +1,65 @@
-let map = document.querySelector('.map-popup');
-let mapButtonClose = document.querySelector('.map-popup-button-close');
-let mapOpen = document.querySelector('.map-popup-open');
+const mapOpen = document.querySelector('.map-popup-open');
+const mapPopup = document.querySelector('.map-popup');
+const mapButtonClose = document.querySelector('.map-popup-button-close');
 
-mapOpen.onclick = function (evt) {
+
+mapOpen.addEventListener("click", function (evt) {
   evt.preventDefault();
-  map.classList.add('show');
-};
+  mapPopup.classList.add('show');
+});
 
-mapButtonClose.onclick = function () {
-  map.classList.remove('show');
-};
-
-let feedbackForm = document.querySelector('.feedback-form');
-let feedbackButtonClose = document.querySelector('.feedback-form-button-close');
-let feedbackButtonOpen = document.querySelector('.feedback-form-open');
-
-feedbackButtonOpen.onclick = function (evt) {
+mapButtonClose.addEventListener("click", function (evt) {
   evt.preventDefault();
-  feedbackForm.classList.add('show');
-};
+  mapPopup.classList.remove('show');
+});
 
-feedbackButtonClose.onclick = function () {
-  feedbackForm.classList.remove('show');
-};
+const feedbackPopup = document.querySelector('.feedback-form');
+const feedbackButtonClose = document.querySelector('.feedback-form-button-close');
+const feedbackButtonOpen = document.querySelector('.feedback-form-open');
+const feedbackFieldName = document.querySelector('[name = name]');
+const feedbackFieldEmail = document.querySelector('[name = email]');
+const feedbackFieldMessage = document.querySelector('[name = message]');
+const feedbackForm = document.querySelector('.feedback-form form');
 
+let isStorageSupport = true;
+let storageName = "";
+let storageEmail = "";
+
+try {
+  storageName = localStorage.getItem('name');
+  storageEmail = localStorage.getItem('email');
+} catch (err) {
+  isStorageSupport = false;
+}
+
+feedbackButtonOpen.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.add('show');
+
+  if (storageName) {
+    feedbackFieldName.value = storageName;
+  }
+
+  if (storageEmail) {
+    feedbackFieldEmail.value = storageEmail;
+  }
+
+  feedbackFieldMessage.focus();
+});
+
+feedbackButtonClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedbackPopup.classList.remove('show');
+});
+
+feedbackForm.addEventListener('submit', function (evt) {
+  if (!feedbackFieldName.value || !feedbackFieldEmail.value || !feedbackFieldMessage.value) {
+    evt.preventDefault();
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem('name', feedbackFieldName.value);
+      localStorage.setItem('email', feedbackFieldEmail.value);
+    }
+  }
+});
 
